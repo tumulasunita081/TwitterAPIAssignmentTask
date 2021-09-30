c63f5cbc1a98c1ff8073a8b94049bf7ea52d2b10
tumulasunita081 <91656931+tumulasunita081@users.noreply.github.com>
2021-09-30 20:04:58 +0530

    Added Project here

---
+++ TwitterAPIAssignment/TwitterAPIAssignment/Models/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace tweeter_data_crawl.Models
+{
+    public class AppSettings
+    {
+        public string MainUserName { get; set; }
+        public long MainUserId { get; set; }
+        public Consumer Consumer { get; set; }
+        public string AuthorizeToken { get; set; }
+    }
+
+    /// <summary>
+    /// https://developer.twitter.com/en/apps/APP_ID
+    /// </summary>
+    public class Consumer
+    {
+        public string Key { get; set; }
+        public string Secret { get; set; }
+    }
+}
